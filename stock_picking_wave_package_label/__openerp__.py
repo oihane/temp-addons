198738fc5d71634b4242eeee5744c7d0e3581c08
oihane <oihanecruce@gmail.com>
2017-05-09 10:41:45 +0200

    [IMP] stock_pickging_wave_package_label: update README and openerp files

--- stock_picking_wave_package_label/__openerp__.py
+++ stock_picking_wave_package_label/__openerp__.py
@@ -4,22 +4,22 @@
 
 {
     "name": "Stock Picking Wave Package Label",
-    "version": "8.0.2.0.0",
-    "depends": [
-        "product_package_label",
-        "stock_picking_wave_management",
-        "base_report_to_printer",
-    ],
-    "author": "OdooMRP team, "
-              "AvanzOSC, "
+    "version": "8.0.1.0.0",
+    "category": "Inventory, Logistic, Storage",
+    "license": "AGPL-3",
+    "author": "AvanzOSC, "
               "Serv. Tecnol. Avanzados - Pedro M. Baeza",
-    "website": "http://www.odoomrp.com",
+    "website": "http://www.avanzosc.es",
     "contributors": [
         "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
         "Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>",
         "Ana Juaristi <anajuaristi@avanzosc.es>",
     ],
-    "category": "Custom Module",
+    "depends": [
+        "product_package_label",
+        "stock_picking_wave_management",
+        "base_report_to_printer",
+    ],
     "data": [
         "security/ir.model.access.csv",
         "views/stock_picking_wave_view.xml",
